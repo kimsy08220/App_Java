3e7f4cc6a34a0480b1d642d0c62e2f09e0f930d8
kimsy08220 <kimsy08220@naver.com>
2021-06-22 02:09:49 +0900

    데이터와 변수

---
+++ Week02/Introduce.java
@@ -0,0 +1,21 @@
+
+public class Introduce {
+
+	public static void main(String[] args) {
+		
+		String name = "김수연", sex = "여", address = "김해", hobby = "유튜브 시청";
+		int age = 22;
+		double cm = 1.58;
+		
+		System.out.println("나에 대해서 소개합니다.");
+		System.out.println("---------------------");
+		System.out.println("이름 : " + name);
+		System.out.println("나이 : " + age);
+		System.out.println("키 : " + cm);
+		System.out.println("성별 : " + sex);
+		System.out.println("사는 곳 : " + address);
+		System.out.println("좋아하는 것 : " + hobby);
+
+	}
+
+}
